1a3499a0ec27d3208af01ebda2777caa3df76096
kjhqwaszx <mju6013@naver.com>
2022-02-04 00:04:40 +0900

    0204 commit

--- src/main.java
+++ src/main.java
@@ -5,12 +5,11 @@ import java.util.*;
 public class main {
 
     public static void main(String[] args) {
-        int[] a1 = {1,2,3,4,5,6};
-        int[] a2 = Arrays.copyOfRange(a1, 0, 2);
+        Integer[] arr = new Integer[3];
+        ArrayList<Integer> arr2 = new ArrayList<Integer>();
+        String str = "1234";
 
-        for (int i = 0; i < a2.length; i++) {
-            System.out.println(a2[i]);
-        }
+
 
     }
 
