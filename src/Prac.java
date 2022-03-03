01cd554f8b9464e183d095367646aaa2c1ce3aad
jaehan.kim <mju6013@naver.com>
2022-03-03 23:13:14 +0900

    0303_현명한 나이트, 결혼식 초대

--- src/Prac.java
+++ src/Prac.java
@@ -2,64 +2,51 @@ import java.util.*;
 
 public class Prac {
     static Scanner sc = new Scanner(System.in);
-    static StringBuilder sb = new StringBuilder();
-
-    static int N,M;
-    static String[] map;
-    static boolean[][] visit;
-    static int[][] dist;
-    static int[][] dir = {{1, 0}, {0, 1}, {-1, 0}, {0, -1}};
+    static int N, M;
+    static int[] map = new int[100001];
+    static boolean[] visit = new boolean[100001];
+    static int[] dist = new int[100001];
 
     static void input() {
         N = sc.nextInt();
         M = sc.nextInt();
-        map = new String[N];
-
-        sc.nextLine();
-        for (int i = 0; i < N; i++) {
-            map[i] = sc.nextLine();
-        }
     }
 
-    static void bfs(int x, int y) {
+    static void bfs(int x) {
         Queue<Integer> que = new LinkedList<>();
+        visit[x] = true;
+        dist[x] = 0;
         que.add(x);
-        que.add(y);
-        visit[x][y] = true;
-        dist[x][y] = 1;
 
         while (!que.isEmpty()) {
             x = que.poll();
-            y = que.poll();
+            if(x == M) break;
 
-            for (int k = 0; k < 4; k++) {
-                int nx = x + dir[k][0];
-                int ny = y + dir[k][1];
-
-                if(nx < 0 || ny < 0 || nx >= N || ny >= M) continue;
-                if(visit[nx][ny]) continue;
-                if(map[nx].charAt(ny) == '0') continue;
-
-                visit[nx][ny] = true;
-                dist[nx][ny] = dist[x][y] +1;
+            int nx = x + 1;
+            if(nx >= 0 && nx <= 100000 && !visit[nx]){
+                visit[nx] = true;
+                dist[nx] = dist[x] + 1;
                 que.add(nx);
-                que.add(ny);
+            }
+
+            nx = x - 1;
+            if(nx >= 0 && nx <= 100000 && !visit[nx]){
+                visit[nx] = true;
+                dist[nx] = dist[x] + 1;
+                que.add(nx);
+            }
+
+            nx = x * 2;
+            if(nx >= 0 && nx <= 100000 && !visit[nx]){
+                visit[nx] = true;
+                dist[nx] = dist[x] + 1;
+                que.add(nx);
             }
         }
     }
-
     static void proc() {
-        visit = new boolean[N][M];
-        dist = new int[N][M];
-
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < M; j++) {
-                dist[i][j] = -1;
-            }
-        }
-        bfs(0,0);
-
-        System.out.println(dist[N-1][M-1]);
+        bfs(N);
+        System.out.println(dist[M]);
     }
     public static void main(String[] args) {
         input();
