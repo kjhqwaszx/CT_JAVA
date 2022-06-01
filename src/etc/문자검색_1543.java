49f88f057da4f08310e4d7baac05b8c8584d046e
kjhqwaszx <kjhqwaszx@naver.com>
2022-06-02 01:16:28 +0900

    0601

--- src/etc/문자검색_1543.java
+++ src/etc/문자검색_1543.java
@@ -1,9 +1,11 @@
+package etc;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.*;
 
-public class Prac {
+public class 문자검색_1543 {
 
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     static String str;
@@ -32,15 +34,12 @@ public class Prac {
     }
 
     public static void main(String[] args) throws IOException {
-        String str = "s!@s";
-        ArrayList<String> tmp = new ArrayList<String>(Arrays.asList(str.split("")));
-        System.out.println(str.contains("6"));
-//        input();
-//        proc();
+
+        input();
+        proc();
 
     }
 
 
 
 }
-
